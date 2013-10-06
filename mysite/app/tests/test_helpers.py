ff05c41d94aabb3a031d2ee5295f968b5b097946
me <me@me>
2013-10-06 18:33:18 -0500

    added t_info statements with more information about individual assertions

--- mysite/app/tests/test_helpers.py
+++ mysite/app/tests/test_helpers.py
@@ -4,30 +4,35 @@ from ttools import t_info
 
 class HelpersTests(TestCase):
     def setUp(self):
-        t_info("TestCase HelpersTests", 1)
-        t_info(self._testMethodName + ": " + self._testMethodDoc, 3)
+        t_info('TestCase HelpersTests', 1)
+        t_info(self._testMethodName + ': ' + self._testMethodDoc, 3)
 
 # TODO: these are just copied from elsewhere, so complete
     def test_helpers(self):
         """
         Test that valid email addresses are parsed from string.
         """
+        t_info('Is empty string parsed correctly?', 5)
         input_string = ''
         correct_out = []
         self.assertEqual(parse_emails(input_string), correct_out)
 
+        t_info('Is a single non-email string parsed correctly?', 5)
         input_string = 'alskjfdlskjflaksjfds'
         correct_out = []
         self.assertEqual(parse_emails(input_string), correct_out)
 
+        t_info('Are multiple emails, separated by punctuation, parsed correctly?', 5)
         input_string = 'one@a.com, two@gmail.com, three@uiuc.edu'
         correct_out = ['one@a.com', 'two@gmail.com', 'three@uiuc.edu']
         self.assertEqual(parse_emails(input_string), correct_out)
 
+        t_info('Is a string with valid as well as invalid emails parsed correctly?', 5) 
         input_string = 'one@a.com, asdf,,,,,,, two@gmail.com'
         correct_out = ['one@a.com', 'two@gmail.com']
         self.assertEqual(parse_emails(input_string), correct_out)
 
+        t_info('Is a string with one invalid email parsed correctly?', 5)
         input_string = 'one@a.c'
         correct_out = []
         self.assertEqual(parse_emails(input_string), correct_out)
