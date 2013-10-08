ccc26db2551b45bea5a807d717d231de12e8c78f
me <me@me>
2013-10-08 12:15:58 -0500

    updated docstrings

--- mysite/app/admin.py
+++ mysite/app/admin.py
@@ -9,7 +9,11 @@ class EmailFormSet(BaseInlineFormSet):
     """ Custom validation. """
 
     def clean(self):
-        """ Custom validation to ensure EndUser not left without any emails """
+        """ Custom validation to ensure EndUser not left without any emails.
+
+        :raises: ValidationError
+        :returns: A dictionary of cleaned form fields
+        """
         # Is the admin trying to delete every email for this enduser? (I.e. is
         # every non-empty dict's DELETE set to True?) If so, create error.
         all_emails = [eu_email for eu_email in self.cleaned_data if eu_email]
@@ -20,13 +24,18 @@ class EmailFormSet(BaseInlineFormSet):
             raise ValidationError('At least one email address is required')
         return self.cleaned_data
 
+
 class EmailInline(admin.TabularInline):
+    """ Options for inline editing of EndUserEmail instances """
+
     model = EndUserEmail
     formset = EmailFormSet
     extra = 2 # by default, provide two email fields
 
+
 class EndUserAdmin(admin.ModelAdmin):
     """ Define options and customization for admin. """
+
     # add ability to edit emails directly on the end user page/form
     inlines = [EmailInline]
 
@@ -47,6 +56,7 @@ class EndUserAdmin(admin.ModelAdmin):
         (None, {'fields': ['first_name', 'last_name']}),
         (None, {'fields': ['street_address', 'city', 'state', 'phone']}),
     ]
+
     def has_add_permission(self, request, obj=None):
         """ Remove admin's ability to add more EndUser objects; don't display
         'Save and Add' button on change_form and 'Add New' button on
