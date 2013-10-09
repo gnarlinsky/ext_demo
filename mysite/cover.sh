3c30fab2604a059a847d7f917ae7e5129d4de77f
me <me@me>
2013-10-09 12:24:38 -0500

    echoing explanatory line

--- mysite/cover.sh
+++ mysite/cover.sh
@@ -6,5 +6,8 @@ coverage run manage.py test --settings=mysite_config_root.settings.test
 coverage report -m --include="app/*" --omit="app/tests/*"
 coverage html --include="app/*" --omit="app/tests/*"
 
+echo ""
+echo "HTML results are in htmlcov/index.html"
+
 # note: only os x
 open htmlcov/index.html
