181f04f512a72f30426d4ddd5893d3ca98945e39
me <me@me>
2013-10-08 21:17:14 -0500

    fix Issue #2

--- mysite/app/admin.py
+++ mysite/app/admin.py
@@ -12,22 +12,25 @@ class EmailFormSet(BaseInlineFormSet):
         """ Custom validation to ensure EndUser not left without any emails.
 
         :raises: ValidationError
-        :returns: A dictionary of cleaned form fields
+        :returns: A dictionary or list of dictionaries of cleaned form fields
         """
         # Is the admin trying to delete every email for this enduser? (I.e. is
         # every non-empty dict's DELETE set to True?) If so, create error.
-        all_emails = [eu_email for eu_email in self.cleaned_data if eu_email]
-        emails_to_del = [eu_email for eu_email in all_emails if eu_email.get('DELETE')]
-        num_emails_to_del = len(emails_to_del)
-        num_all_emails = len(all_emails)
-        if num_all_emails == num_emails_to_del:
+        try:
+            all_emails = [eu_email for eu_email in self.cleaned_data if eu_email]
+            emails_to_del = [eu_email for eu_email in all_emails if eu_email.get('DELETE')]
+            num_emails_to_del = len(emails_to_del)
+            num_all_emails = len(all_emails)
+            if num_all_emails == num_emails_to_del:
+                raise ValidationError('At least one email address is required')
+        except:
             raise ValidationError('At least one email address is required')
         return self.cleaned_data
 
 
 class EmailInline(admin.TabularInline):
-    """ Options for inline editing of EndUserEmail instances """
-
+    """ Options for inline editing of EndUserEmail instances
+    """
     model = EndUserEmail
     formset = EmailFormSet
     extra = 2 # by default, provide two email fields
