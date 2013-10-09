3d010a1e5d0d4a66d0bdb302ef5216b9e68ee3b7
me <me@me>
2013-10-08 19:55:39 -0500

    clean up/organize and cleaner redirection to change_list

--- mysite/mysite_config_root/urls.py
+++ mysite/mysite_config_root/urls.py
@@ -2,11 +2,12 @@ from app import views
 from django.conf.urls import patterns, include, url
 # enable admin
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^signup/', views.signup, name='signup'),
     url(r'^signupadmin/', views.signup_admin),
-    url(r'^signup_admin/', views.signup_admin),
+    url(r'^admin/$', lambda x: HttpResponseRedirect('/admin/app/enduser')),
     url(r'^admin/', include(admin.site.urls)),
 )
